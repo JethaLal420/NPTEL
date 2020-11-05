52e7edd9ba0c50eb553483a23ed11f6cc8602891
Jay <42314425+JethaLal420@users.noreply.github.com>
2020-11-05 18:06:51 +0530

    Initial commit

---
+++ The Joy of computing using python/Week 8 Ass. 1 - Repeat.py
@@ -0,0 +1,36 @@
+# Write a Python program to add the digits of a positive
+#  integer repeatedly until the result has a single digit.
+
+# Input Format:
+# The first line of the input contains a number n.
+
+# Output:
+# Print the resultant number
+
+# Example:
+# Input:
+# 48
+
+# Output:
+# 3
+
+# Explanation: If you add digits 4 and 8, you will get 12. 
+# Again adding 1 and 2 will give 3 which is a single digit and hence the answer.
+
+
+
+
+if __name__ == "__main__":
+    n = int(input())
+    while(True):
+        sum = 0
+        while(n > 0):
+            tmp = n%10
+            sum = sum + tmp
+            n = n // 10
+        if sum < 10:
+            n = sum
+            break
+        else:
+            n = sum
+    print(n, end="")
---
+++ The Joy of computing using python/Week 8 Ass. 2 - Push.py
@@ -0,0 +1,42 @@
+# Write a Python program to push all zeros to the end of a given list a. 
+# The order of the elements should not change.
+
+# Input Format:
+# Elements of the list a with each element separated by a space.
+
+# Output Format:
+# Elements of the modified list with each element separated by a space. 
+# After the last element, there should not be any space.
+
+# Example:
+
+# Input:
+# 0 2 3 4 6 7 10
+
+# Output:
+# 2 3 4 6 7 10 0
+
+# Explanation:
+# There is one zero in the list. 
+# After pushing it at the end the elements of the list becomes 2 3 4 6 7 10 0. 
+# The order of other elements remains the same.
+
+
+
+
+
+n = input().split()
+
+counter = 0
+
+for i in n:
+    if i == '0':
+        n.remove(i)
+        counter += 1
+        n.append(i)
+
+for i in range(0, len(n)):
+    if i == len(n) - 1:
+        print(int(n[i]), end = "")
+    else:
+        print(int(n[i]), end = " ")
