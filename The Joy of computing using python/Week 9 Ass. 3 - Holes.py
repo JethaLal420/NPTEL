776eda08a5129667ff90d2ba9275845d7dcc968e
Jay <42314425+JethaLal420@users.noreply.github.com>
2020-11-12 13:53:33 +0530

    Update Week 9 Ass. 3 - Holes.py

--- The Joy of computing using python/Week 9 Ass. 3 - Holes.py
+++ The Joy of computing using python/Week 9 Ass. 3 - Holes.py
@@ -1,24 +1,24 @@
 # # The Joy of Computing using Python Week 9 (12/11/2020)
 
-Let us assume paper as the plane and a letter as a curve on the plane, then each letter divides the plane into regions. For example letters "A", "D", "O", "P", "R" divide the plane into two regions so we say these letters each have one hole. Similarly, letter "B" has two holes and letters such as "C", "E", "F", "K" have no holes. We say that the number of holes in the text is equal to the total number of holes in the letters of the text. Write a program to determine how many holes are in a given text.
+# Let us assume paper as the plane and a letter as a curve on the plane, then each letter divides the plane into regions. For example letters "A", "D", "O", "P", "R" divide the plane into two regions so we say these letters each have one hole. Similarly, letter "B" has two holes and letters such as "C", "E", "F", "K" have no holes. We say that the number of holes in the text is equal to the total number of holes in the letters of the text. Write a program to determine how many holes are in a given text.
 
-Input Format:
-The only line contains a non-empty text composed only of uppercase letters of English alphabet.
+# Input Format:
+# The only line contains a non-empty text composed only of uppercase letters of English alphabet.
 
-Output Format:
-output a single line containing the number of holes in the corresponding text.
+# Output Format:
+# output a single line containing the number of holes in the corresponding text.
 
-Example-1
+# Example-1
 
-Input:
-DRINKEATCODE
+# Input:
+# DRINKEATCODE
 
-Output:
-5
+# Output:
+# 5
 
 
-Explanation:
-D R A O D has one hole hence total number of holes in the text is 5.
+# Explanation:
+# D R A O D has one hole hence total number of holes in the text is 5.
 
 
 
