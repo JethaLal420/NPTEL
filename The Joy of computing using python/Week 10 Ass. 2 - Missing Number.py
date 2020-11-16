ec61b984123e2bc011d0203598010382dde63c9e
Jay <42314425+JethaLal420@users.noreply.github.com>
2020-11-16 17:31:01 +0530

    Update Week 10 Ass. 2 - Missing Number.py

--- The Joy of computing using python/Week 10 Ass. 2 - Missing Number.py
+++ The Joy of computing using python/Week 10 Ass. 2 - Missing Number.py
@@ -2,24 +2,24 @@
 
 
 
-Given a list of n-1 numbers ranging from 1 to n, your task is to find the missing number. There are no duplicates. 
+# Given a list of n-1 numbers ranging from 1 to n, your task is to find the missing number. There are no duplicates. 
 
-Input Format:
-The first line contains n-1 numbers with each number separated by a space.
+# Input Format:
+# The first line contains n-1 numbers with each number separated by a space.
 
-Output Format:
-Print the missing number
+# Output Format:
+# Print the missing number
 
-Example:
+# Example:
 
-Input:
-1 2 4 6 3 7 8
+# Input:
+# 1 2 4 6 3 7 8
 
-Output:
-5
+# Output:
+# 5
 
-Explanation:
-In the above list of numbers 5 is missing and hence 5 is the input
+# Explanation:
+# In the above list of numbers 5 is missing and hence 5 is the input
 
 
 
@@ -27,6 +27,7 @@ def func(n):
     n.sort()
     sum2 = 0
     sum1 = ( int(n[-1] ) * ( int(n[-1]) + 1 )) //2
+    
     for i in range(0, len(n)):
         sum2 = sum2 + int(n[i])
 
