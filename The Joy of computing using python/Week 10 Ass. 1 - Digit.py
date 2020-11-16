5c752deb77cb1769e02015c5c0244eede259f4e2
Jay <42314425+JethaLal420@users.noreply.github.com>
2020-11-16 17:34:40 +0530

    Update Week 10 Ass. 1 - Digit.py

--- The Joy of computing using python/Week 10 Ass. 1 - Digit.py
+++ The Joy of computing using python/Week 10 Ass. 1 - Digit.py
@@ -1,33 +1,34 @@
 # # The Joy of Computing using Python Week 10 (16 Nov 2020)
 
 
-You are provided with a number D containing only digits 0's and 1's. Your aim is to convert this number to have all the digits same.
-For that, you will change exactly one digit i.e. from 0 to 1 or from 1 to 0. If it is possible to make all digits equal (either all 0's or all 1's) by flipping exactly 1 digit then output "YES", else print "NO" (quotes for clarity).
+# You are provided with a number D containing only digits 0's and 1's. Your aim is to convert this number to have all the digits same.
+# For that, you will change exactly one digit i.e. from 0 to 1 or from 1 to 0. If it is possible to make all digits equal (either all 0's or all 1's) by 
+#     flipping exactly 1 digit then output "YES", else print "NO" (quotes for clarity).
 
-Input Format:
-The first line of the input contains the number D made of only digits 1's and 0's.
+# Input Format:
+# The first line of the input contains the number D made of only digits 1's and 0's.
 
-Output:
-Print 'YES' or 'NO' depending on whether its possible to make it all 0s or 1s or not. 
+# Output:
+# Print 'YES' or 'NO' depending on whether its possible to make it all 0s or 1s or not. 
 
-Example-1:
+# Example-1:
 
-Input:
-101
+# Input:
+# 101
 
-Output:
-YES
+# Output:
+# YES
 
-Example-2:
+# Example-2:
 
-Input:
-11
+# Input:
+# 11
 
-Output:
-NO
+# Output:
+# NO
 
-Explanation:
-In the first example, it is possible to make all the digits same by flipping the middle digit from 0 to 1. In the second example it is not possible.
+# Explanation:
+# In the first example, it is possible to make all the digits same by flipping the middle digit from 0 to 1. In the second example it is not possible.
 
 
 
