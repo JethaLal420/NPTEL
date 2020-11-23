72f84cb75c4e5584fb913c5d2bd448d412af4135
Jay <42314425+JethaLal420@users.noreply.github.com>
2020-11-23 15:04:01 +0530

    Update Week 11 Ass. 1 - Formula.py

--- The Joy of computing using python/Week 11 Ass. 1 - Formula.py
+++ The Joy of computing using python/Week 11 Ass. 1 - Formula.py
@@ -1,25 +1,25 @@
 # # The Joy of Computing using Python Week 11 (23 Nov 2020)
 
 
-Write a program that calculates and prints the value according to the given formula:
-Q = Square root of [(2 * C * D)/H]
-Following are the fixed values of C and H:
-C is 50. H is 30.
-D is the variable whose values should be input to your program in a comma-separated sequence.
+# Write a program that calculates and prints the value according to the given formula:
+# Q = Square root of [(2 * C * D)/H]
+# Following are the fixed values of C and H:
+# C is 50. H is 30.
+# D is the variable whose values should be input to your program in a comma-separated sequence.
 
-Input Format:
-A sequence of values for D with each value separated by a comma.
+# Input Format:
+# A sequence of values for D with each value separated by a comma.
 
-Output Format:
-Print the sequence of Q values with each value separated by a comma.
+# Output Format:
+# Print the sequence of Q values with each value separated by a comma.
 
-Example:
+# Example:
 
-Input:
-100,150,180
+# Input:
+# 100,150,180
 
-Output:
-18,22,24
+# Output:
+# 18,22,24
 
 
 import math
