68dff9c5124e5c61342e00a6a4110858e0fb48fb
Jay <42314425+JethaLal420@users.noreply.github.com>
2020-11-23 15:31:37 +0530

    Update Week 11 Ass. 3 - Numbers.py

--- The Joy of computing using python/Week 11 Ass. 3 - Numbers.py
+++ The Joy of computing using python/Week 11 Ass. 3 - Numbers.py
@@ -1,17 +1,17 @@
 # # The Joy of Computing using Python Week 11 (23 Nov 2020)
 
 
-Write a program, which will find all such numbers between m and n (both included) such that each digit of the number is an even number.
+# Write a program, which will find all such numbers between m and n (both included) such that each digit of the number is an even number.
 
-Input Format:
-The first line contains value m and n separated by a comma.
+# Input Format:
+# The first line contains value m and n separated by a comma.
 
-Output Format:
-The numbers obtained should be printed in a comma-separated sequence on a single line.
+# Output Format:
+# The numbers obtained should be printed in a comma-separated sequence on a single line.
 
-Constraints:
-1000<=m<=9000
-1000<=n<=9000
+# Constraints:
+# 1000<=m<=9000
+# 1000<=n<=9000
 
 
 
