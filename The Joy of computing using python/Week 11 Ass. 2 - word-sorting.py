d76a465655649cdf95a3441f375df11d29b23858
Jay <42314425+JethaLal420@users.noreply.github.com>
2020-11-23 15:08:30 +0530

    Update Week 11 Ass. 2 - word-sorting.py

--- The Joy of computing using python/Week 11 Ass. 2 - word-sorting.py
+++ The Joy of computing using python/Week 11 Ass. 2 - word-sorting.py
@@ -1,20 +1,20 @@
 # # The Joy of Computing using Python Week 11 (23 Nov 2020)
 
-Write a program that accepts a comma-separated sequence of words as input and prints the words in a comma-separated sequence after sorting them alphabetically.
+# Write a program that accepts a comma-separated sequence of words as input and prints the words in a comma-separated sequence after sorting them alphabetically.
 
-Input Format:
-The first line of input contains words separated by the comma.
+# Input Format:
+# The first line of input contains words separated by the comma.
 
-Output Format:
-Print the sorted words separated by the comma.
+# Output Format:
+# Print the sorted words separated by the comma.
 
-Example:
+# Example:
 
-Input:
-without,hello,bag,world
+# Input:
+# without,hello,bag,world
 
-Output:
-bag,hello,without,world
+# Output:
+# bag,hello,without,world
 
 
 
