0304b879bd180a0799d3586f9a53c9d2e166866b
Jay <42314425+JethaLal420@users.noreply.github.com>
2020-12-04 19:45:03 +0530

    Create Week 12 Ass. 1 - Sentence.py

---
+++ The Joy of computing using python/Week 12 Ass. 1 - Sentence.py
@@ -0,0 +1,38 @@
+# # The Joy of Computing using Python Week 12 (04 Dec 2020)
+
+
+Write a program that accepts sequence of lines as input and prints the lines after making all characters in the sentence capitalised.
+
+Input Format:
+The first line of the input contains a number n which represents the number of line.
+From second line there are statements which has to be converted. Each statement comes in a new line.
+
+Output Format:
+Print statements with each word in capital letters.
+
+Example:
+
+Input:
+2
+Hello world
+Practice makes perfect
+
+Output:
+HELLO WORLD
+PRACTICE MAKES PERFECT
+
+
+
+n = int(input())
+
+l = []
+
+for i in range(0, n):
+    j = input()
+    l.append(j)
+
+for i in range(0, n):
+    if i != n-1:
+        print(l[i].upper())
+    else:
+        print(l[i].upper(), end = '')
